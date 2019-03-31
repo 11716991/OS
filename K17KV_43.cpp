1fa7eb6c760f2ee2be85114e38287811fb1c63dd
11716991 <48068262+11716991@users.noreply.github.com>
2019-03-31 11:12:28 +0530

    Update K17KV_43.cpp

--- K17KV_43.cpp
+++ K17KV_43.cpp
@@ -11,7 +11,7 @@ scanf("%d",&mx);
 printf("\nEnter the size of the Array:-\t\t");
 scanf("%d",&n);
 printf("\nEnter the array elements:-\n");
-for(i=1;i<=n;i++)
+for(i=1;i<n;i++)
 {
 a:
 printf("\nEnter the %d element:-\t\t",i); 
@@ -23,8 +23,16 @@ if(array[i]>=mx)
  goto a;	
 }
 }
+b:
 printf("\nEnter the initial head position:-\t\t");
 scanf("%d",&head);
+if(head>mx||head<0)
+{
+ printf("!!Error 505!!\n");
+ printf("Head cannot be greater than %d and less than zero\n");
+ printf("!!!!Enter again!!!!");
+}
+goto b;
 array[0]=head;
 for(j=0;j<=n-1;j++)
 {
